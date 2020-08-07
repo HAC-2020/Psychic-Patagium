1454568437c040fd84507bf2bfefe050657404ae
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-08-07 20:12:22 +0530

    feat: add URLs and templates for Users

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", views.signup_page, name="signup"),
+    path(
+        "accounts/signup/customer/",
+        views.CustomerSignUpView.as_view(),
+        name="customer_signup",
+    ),
+    path(
+        "accounts/signup/shopowner/",
+        views.ShopOwnerSignUpView.as_view(),
+        name="shopowner_signup",
+    ),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CustomerSignUpForm, ShopOwnerSignUpForm
+from .models import User
+
+
+def signup_page(request):
+    return render(request, "registration/signup.html")
+
+
+class CustomerSignUpView(CreateView):
+    model = User
+    form_class = CustomerSignUpForm
+    template_name = "registration/signup_form.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect("location_select")
+
+
+class ShopOwnerSignUpView(CreateView):
+    model = User
+    form_class = ShopOwnerSignUpForm
+    template_name = "registration/signup_form.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect("shop_select")
+
